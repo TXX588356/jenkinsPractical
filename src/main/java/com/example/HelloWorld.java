3e3e1430ba064acb89644b9b39414542f328820e
LeeTianXin <leetianxin04@gmail.com>
2025-08-30 12:16:45 +0800

    tryin

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello there Jenkins Pumpkins, Napkins testing testing testing!!!"); //comment
+        System.out.println("Hello, World!"); //comment
     }
 
     public String getGreeting() {
-        return "Hello, World!!!!";
+        return "Hello, World!";
     }
 }
